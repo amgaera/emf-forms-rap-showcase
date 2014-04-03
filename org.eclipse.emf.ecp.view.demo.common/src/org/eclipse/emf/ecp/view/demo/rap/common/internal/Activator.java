dfde422196850492989a72d7a1c8da43bfadf2d7
Artur Grunau <artur.grunau@tum.de>
2014-04-03 17:15:05 +0200

    Update references to the RAP application

--- org.eclipse.emf.ecp.view.demo.common/src/org/eclipse/emf/ecp/view/demo/rap/common/internal/Activator.java
+++ org.eclipse.emf.ecp.view.demo.common/src/org/eclipse/emf/ecp/view/demo/rap/common/internal/Activator.java
@@ -3,35 +3,36 @@ package org.eclipse.emf.ecp.view.demo.rap.common.internal;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.emf.forms.main.IExampleContribution;
+import org.eclipse.emf.ecp.view.demo.rap.IExampleContribution;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
-
 
 public class Activator implements BundleActivator {
 
 	private static final String EXAMPLE_CONTRIB = IExampleContribution.class.getName();
 
-	  private final Contributions contibutions;
-	  private final List<ServiceRegistration<?>> registrations;
+	private final Contributions contibutions;
+	private final List<ServiceRegistration<?>> registrations;
 
-	  public Activator() {
-	    contibutions = new Contributions();
-	    registrations = new ArrayList<ServiceRegistration<?>>();
-	  }
+	public Activator() {
+		contibutions = new Contributions();
+		registrations = new ArrayList<ServiceRegistration<?>>();
+	}
 
-	  public void start( BundleContext context ) throws Exception {
-	    for( IExampleContribution contribution : contibutions.getContibutions() ) {
-	      ServiceRegistration<?> registration;
-	      registration = context.registerService( EXAMPLE_CONTRIB, contribution, null );
-	      registrations.add( registration );
-	    }
-	  }
+	@Override
+	public void start(BundleContext context) throws Exception {
+		for (final IExampleContribution contribution : contibutions.getContibutions()) {
+			ServiceRegistration<?> registration;
+			registration = context.registerService(EXAMPLE_CONTRIB, contribution, null);
+			registrations.add(registration);
+		}
+	}
 
-	  public void stop( BundleContext context ) throws Exception {
-	    for( ServiceRegistration<?> registration : registrations ) {
-	      registration.unregister();
-	    }
-	  }
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		for (final ServiceRegistration<?> registration : registrations) {
+			registration.unregister();
+		}
+	}
 }
--- org.eclipse.emf.ecp.view.demo.common/src/org/eclipse/emf/ecp/view/demo/rap/common/internal/Contributions.java
+++ org.eclipse.emf.ecp.view.demo.common/src/org/eclipse/emf/ecp/view/demo/rap/common/internal/Contributions.java
@@ -6,7 +6,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    EclipseSource - initial API and implementation
+ * EclipseSource - initial API and implementation
  ******************************************************************************/
 package org.eclipse.emf.ecp.view.demo.rap.common.internal;
 
@@ -14,53 +14,52 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.eclipse.emf.ecp.view.demo.rap.IExampleContribution;
+import org.eclipse.emf.ecp.view.demo.rap.IExamplePage;
 import org.eclipse.emf.ecp.view.demo.rap.common.ButtonExamplePage;
 import org.eclipse.emf.ecp.view.demo.rap.common.TablesExamplePage;
-import org.eclipse.emf.forms.main.IExampleContribution;
-import org.eclipse.emf.forms.main.IExamplePage;
-
 
 class Contributions {
 
-  private final List<IExampleContribution> contributions;
+	private final List<IExampleContribution> contributions;
 
-  Contributions() {
-    contributions = new ArrayList<IExampleContribution>();
-    collectContributions();
-  }
+	Contributions() {
+		contributions = new ArrayList<IExampleContribution>();
+		collectContributions();
+	}
 
-  List<IExampleContribution> getContibutions() {
-    return Collections.unmodifiableList( contributions );
-  }
+	List<IExampleContribution> getContibutions() {
+		return Collections.unmodifiableList(contributions);
+	}
 
-  private void collectContributions() {
-    addContribution( "button", "Buttons", ButtonExamplePage.class );
-    addContribution( "table-template", "Tables", TablesExamplePage.class );
-  }
+	private void collectContributions() {
+		addContribution("button", "Buttons", ButtonExamplePage.class);
+		addContribution("table-template", "Tables", TablesExamplePage.class);
+	}
 
-  private void addContribution( final String id,
-                                final String title,
-                                final Class<? extends IExamplePage> clazz )
-  {
-    IExampleContribution contribution = new IExampleContribution() {
+	private void addContribution(final String id,
+		final String title,
+		final Class<? extends IExamplePage> clazz)
+	{
+		final IExampleContribution contribution = new IExampleContribution() {
 
-      public String getId() {
-        return id;
-      }
+			public String getId() {
+				return id;
+			}
 
-      public String getTitle() {
-        return title;
-      }
+			public String getTitle() {
+				return title;
+			}
 
-      public IExamplePage createPage() {
-        try {
-          return clazz.newInstance();
-        } catch( Exception exception ) {
-          throw new RuntimeException( "Failed to instatiate class " + clazz.getName(), exception );
-        }
-      }
-    };
-    contributions.add( contribution );
-  }
+			public IExamplePage createPage() {
+				try {
+					return clazz.newInstance();
+				} catch (final Exception exception) {
+					throw new RuntimeException("Failed to instatiate class " + clazz.getName(), exception);
+				}
+			}
+		};
+		contributions.add(contribution);
+	}
 
 }
